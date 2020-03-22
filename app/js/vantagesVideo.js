85a4d20a8d68d13afda94af600a3ffd1f6461b95
Fedya Tsarenko <fedya132465@gmail.com>
2020-03-22 13:39:26 +0200

    --

--- app/js/vantagesVideo.js
+++ app/js/vantagesVideo.js
@@ -1,7 +1,7 @@
 (function() {
    $(document).ready(function() {
       $('.vantage_video-slider').slick({
-         slidesToShow: 1,
+         slidesToShow: 3,
          slidesToScroll: 1,
          adaptiveHeight: true,
          prevArrow: $('.vantage_video-previous-button'),
@@ -9,6 +9,12 @@
          dots: true,
          appendDots: $('.vantage_main-video-container'),
          dotsClass: 'slider-dots',
+         responsive: [ {
+            breakpoint: 1134,
+            settings: {
+               slidesToShow: 1
+            }
+         }]
       });
    });
 })();
